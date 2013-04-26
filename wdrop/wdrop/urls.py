b161d265c001b61da700497158cd268341a6fd5d
André Gonçalves <andreslb1@gmail.com>
2013-04-26 01:32:50 +0100

    rtc data channel working test

--- wdrop/wdrop/urls.py
+++ wdrop/wdrop/urls.py
@@ -2,18 +2,11 @@ from django.conf.urls import patterns, include, url
 from webapp import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name="index")
-    # Examples:
-    # url(r'^$', 'wdrop.views.home', name='home'),
-    # url(r'^wdrop/', include('wdrop.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', views.index, name="index"),
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^admin/', include(admin.site.urls))
 )
---
+++ wdrop/webapp/static/js/app.js
@@ -0,0 +1,77 @@
+/* Data Channel tests - Send file */
+
+var panel = document.getElementById('panel');
+var status_msg = document.getElementById('status_msg');
+var packets = document.getElementById('packets');
+var channel = new DataChannel();
+var sendfile = document.getElementById('sendfile');
+
+channel.firebase = 'webrtc-experiment';
+
+channel.onmessage = function(data) {
+    console.log("new message");
+    console.log(data);
+};
+
+channel.onopen = function () {
+    sendfile.disabled = false;
+    status_msg.innerHTML = "Connected!";
+    $("#packets").show();
+};
+
+channel.onFileProgress = function (packets) {
+	appendPanel(packets.remaining + ' packets remaining.');
+    if (packets.sent) {
+        appendPanel(packets.sent + ' packets sent.');
+    }
+    if (packets.received) {
+        appendPanel(packets.received + ' packets received.');
+    }
+};
+
+channel.onFileReceived = function (fileName) {
+	status_msg.innerHTML = "File received!";
+};
+
+channel.onFileSent = function (file) {
+    appendPanel(file.name + ' sent.');
+};
+
+sendfile.onchange = function () {
+    console.log("changed");
+    var file = this.files[0];
+    channel.send(file);
+};
+
+channel.onerror = function(event) {
+    console.log(event);
+    status_msg.innerHTML = "Error!";
+};
+
+channel.onclose = function(event) {
+    status_msg.innerHTML = "Ports dropped!";
+};
+
+function openChannel() {
+	console.log("try to open channel");
+	channel.open('cenas');
+    console.log(channel);
+}
+
+function connectChannel() {
+    console.log("try to connect");
+	channel.connect('cenas');
+    console.log(channel);
+}
+
+function sendText(text) {
+	channel.send(text);
+}
+
+function appendPanel(data, parent) {
+    var html = data;
+    html += "<br>" + panel.innerHTML;
+    panel.innerHTML = html;
+}
+
+
